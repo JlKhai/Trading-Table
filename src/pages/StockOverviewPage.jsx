07827962ae522cc847626adf3e46f1d742d06a77
unknown <jlzamliankhai@gmail.com>
2023-09-01 00:16:24 +0630

    fixed responsive

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -4,7 +4,7 @@ import StockList from "../components/StockList";
 
 const StockOverviewPage = () => {
   return (
-    <div className="flex justify-center flex-col items-center">
+    <div className="flex justify-center flex-col items-center h-screen">
       <img
         src={
           "https://i.pinimg.com/236x/a9/76/02/a9760243f94345114cdf70626795ebcc.jpg"
